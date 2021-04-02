fb56c7fd70ca7c94c58c6b8f55f45261b6c1a4d1
UMER DOGAR <31399606+umerdogar@users.noreply.github.com>
2021-04-02 10:58:48 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Code od Quotient and remainder for applied programming
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		int num1 = 15, num2 = 2;
+        int quotient = num1 / num2;
+        int remainder = num1 % num2;
+        System.out.println("Quotient is: " + quotient);
+        System.out.println("Remainder is: " + remainder);
+	}
+}
+
+
